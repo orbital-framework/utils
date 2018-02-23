7cd160a26f54137d0f6303f7bfe260dd5828f5a6
Mateus Souza <mateussouzaweb@gmail.com>
2018-02-23 17:45:20 -0300

    Tabs to spaces conversion

--- app/Libraries/Input.php
+++ app/Libraries/Input.php
@@ -2,180 +2,180 @@
 
 class Input {
 
-	/**
-	 * $_POST
-	 * @var array
-	 */
-	private $_post = array();
+    /**
+     * $_POST
+     * @var array
+     */
+    private $_post = array();
 
-	/**
-	 * $_GET
-	 * @var array
-	 */
-	private $_get = array();
+    /**
+     * $_GET
+     * @var array
+     */
+    private $_get = array();
 
-	/**
-	 * $_PUT
-	 * @var array
-	 */
-	private $_put = array();
+    /**
+     * $_PUT
+     * @var array
+     */
+    private $_put = array();
 
-	/**
-	 * $_DELETE
-	 * @var array
-	 */
-	private $_delete = array();
+    /**
+     * $_DELETE
+     * @var array
+     */
+    private $_delete = array();
 
-	/**
-	 * $_FILES
-	 * @var array
-	 */
-	private $_files = array();
+    /**
+     * $_FILES
+     * @var array
+     */
+    private $_files = array();
 
-	/**
-	 * CONSTRUCTOR
-	 * Proccess inputed data
-	 */
-	public function __construct() {
+    /**
+     * CONSTRUCTOR
+     * Process input data
+     */
+    public function __construct() {
 
-		if( isset($_POST) ){
-			foreach( $_POST as $key => $value ){
-				if( !is_null($value) ){
-					$this->_post[ $key ] = $value;
-				}
-			}
-		}
+        if( isset($_POST) ){
+            foreach( $_POST as $key => $value ){
+                if( !is_null($value) ){
+                    $this->_post[ $key ] = $value;
+                }
+            }
+        }
 
-		if( isset($_GET) ){
-			foreach( $_GET as $key => $value ){
-				if( !is_null($value) ){
-					$this->_get[ $key ] = $value;
-				}
-			}
-		}
+        if( isset($_GET) ){
+            foreach( $_GET as $key => $value ){
+                if( !is_null($value) ){
+                    $this->_get[ $key ] = $value;
+                }
+            }
+        }
 
-		if( isset($_FILES) ){
-			foreach( $_FILES as $key => $value ){
+        if( isset($_FILES) ){
+            foreach( $_FILES as $key => $value ){
 
-				if( is_null($value) ){
-					continue;
-				}
+                if( is_null($value) ){
+                    continue;
+                }
 
-				$data = array();
+                $data = array();
 
-				foreach( $value as $subkey => $subvalue ){
-					if( is_array($subvalue) ){
-						foreach( $subvalue as $subfile => $subfilevalue ){
-							$data[ $subfile ][ $subkey ] = $subfilevalue;
-						}
-					}else{
-						$data[ $subkey ] = $subvalue;
-					}
-				}
+                foreach( $value as $subKey => $subValue ){
+                    if( is_array($subValue) ){
+                        foreach( $subValue as $subFile => $subFileValue ){
+                            $data[ $subFile ][ $subKey ] = $subFileValue;
+                        }
+                    }else{
+                        $data[ $subKey ] = $subValue;
+                    }
+                }
 
-				$this->_files[ $key ] = $data;
-			}
-		}
+                $this->_files[ $key ] = $data;
+            }
+        }
 
-		switch( $_SERVER['REQUEST_METHOD'] ){
-		case 'PUT':
-			parse_str( file_get_contents( 'php://input' ), $this->_put );
-		break;
-		case 'DELETE':
-			parse_str( file_get_contents( 'php://input' ), $this->_delete );
-		break;
-		}
+        switch( $_SERVER['REQUEST_METHOD'] ){
+        case 'PUT':
+            parse_str( file_get_contents( 'php://input' ), $this->_put );
+        break;
+        case 'DELETE':
+            parse_str( file_get_contents( 'php://input' ), $this->_delete );
+        break;
+        }
 
-	}
+    }
 
-	/**
-	 * Retrieve values sent by $_POST
-	 * @param string $key
-	 * @return mixed
-	 */
-	public function post($key = NULL){
+    /**
+     * Retrieve values sent by $_POST
+     * @param string $key
+     * @return mixed
+     */
+    public function post($key = NULL){
 
-		if( is_null($key) ){
-			return $this->_post;
-		}
+        if( is_null($key) ){
+            return $this->_post;
+        }
 
-		if( isset($this->_post[ $key ]) ){
-			return $this->_post[ $key ];
-		}
+        if( isset($this->_post[ $key ]) ){
+            return $this->_post[ $key ];
+        }
 
-		return FALSE;
-	}
+        return FALSE;
+    }
 
-	/**
-	 * Retrieve values sent by $_GET
-	 * @param string $key
-	 * @return mixed
-	 */
-	public function get($key = NULL){
+    /**
+     * Retrieve values sent by $_GET
+     * @param string $key
+     * @return mixed
+     */
+    public function get($key = NULL){
 
-		if( is_null($key) ){
-			return $this->_get;
-		}
+        if( is_null($key) ){
+            return $this->_get;
+        }
 
-		if( isset($this->_get[ $key ]) ){
-			return $this->_get[ $key ];
-		}
+        if( isset($this->_get[ $key ]) ){
+            return $this->_get[ $key ];
+        }
 
-		return FALSE;
-	}
+        return FALSE;
+    }
 
-	/**
-	 * Retrieve values sent by $_PUT
-	 * @param string $key
-	 * @return mixed
-	 */
-	public function put($key = NULL){
+    /**
+     * Retrieve values sent by $_PUT
+     * @param string $key
+     * @return mixed
+     */
+    public function put($key = NULL){
 
-		if( is_null($key) ){
-			return $this->_put;
-		}
+        if( is_null($key) ){
+            return $this->_put;
+        }
 
-		if( isset($this->_put[ $key ]) ){
-			return $this->_put[ $key ];
-		}
+        if( isset($this->_put[ $key ]) ){
+            return $this->_put[ $key ];
+        }
 
-		return FALSE;
-	}
+        return FALSE;
+    }
 
-	/**
-	 * Retrieve values sent by $_DELETE
-	 * @param string $key
-	 * @return mixed
-	 */
-	public function delete($key = NULL){
+    /**
+     * Retrieve values sent by $_DELETE
+     * @param string $key
+     * @return mixed
+     */
+    public function delete($key = NULL){
 
-		if( is_null($key) ){
-			return $this->_delete;
-		}
+        if( is_null($key) ){
+            return $this->_delete;
+        }
 
-		if( isset($this->_delete[ $key ]) ){
-			return $this->_delete[ $key ];
-		}
+        if( isset($this->_delete[ $key ]) ){
+            return $this->_delete[ $key ];
+        }
 
-		return FALSE;
-	}
+        return FALSE;
+    }
 
-	/**
-	 * Retrieve values sent by $_FILES
-	 * @param string $key
-	 * @return mixed
-	 */
-	public function files($key = NULL){
+    /**
+     * Retrieve values sent by $_FILES
+     * @param string $key
+     * @return mixed
+     */
+    public function files($key = NULL){
 
-		if( is_null($key) ){
-			return $this->_files;
-		}
+        if( is_null($key) ){
+            return $this->_files;
+        }
 
-		if( isset($this->_files[ $key ]) ){
-			return $this->_files[ $key ];
-		}
+        if( isset($this->_files[ $key ]) ){
+            return $this->_files[ $key ];
+        }
 
-		return FALSE;
-	}
+        return FALSE;
+    }
 
 }
--- app/Libraries/Resize.php
+++ app/Libraries/Resize.php
@@ -2,346 +2,346 @@
 
 class Resize {
 
-	private $image;
-	private $width;
-	private $height;
-	private $imageResized;
+    private $image;
+    private $width;
+    private $height;
+    private $imageResized;
 
-	/**
-	 * CONSTRUCTOR
-	 * @param string $file
-	 */
-	public function __construct($file){
+    /**
+     * CONSTRUCTOR
+     * @param string $file
+     */
+    public function __construct($file){
 
-		$this->image = $this->openImage($file);
-		$this->width  = imagesx($this->image);
-		$this->height = imagesy($this->image);
+        $this->image = $this->openImage($file);
+        $this->width  = imagesx($this->image);
+        $this->height = imagesy($this->image);
 
-	}
+    }
 
-	/**
-	 * Open image for resizing
-	 * @param string $file
-	 * @return resource
-	 */
-	private function openImage($file){
+    /**
+     * Open image for resizing
+     * @param string $file
+     * @return resource
+     */
+    private function openImage($file){
 
-		$extension = strtolower(strrchr($file, '.'));
+        $extension = strtolower(strrchr($file, '.'));
 
-		switch( $extension ){
-			case '.jpg':
-			case '.jpeg':
-				$img = @imagecreatefromjpeg($file);
-			break;
-			case '.gif':
-				$img = @imagecreatefromgif($file);
-			break;
-			case '.png':
-				$img = @imagecreatefrompng($file);
-			break;
-			default:
-				$img = false;
-			break;
-		}
+        switch( $extension ){
+            case '.jpg':
+            case '.jpeg':
+                $img = @imagecreatefromjpeg($file);
+            break;
+            case '.gif':
+                $img = @imagecreatefromgif($file);
+            break;
+            case '.png':
+                $img = @imagecreatefrompng($file);
+            break;
+            default:
+                $img = false;
+            break;
+        }
 
-		$img = $this->adjustImageOrientation($file, $img);
+        $img = $this->adjustImageOrientation($file, $img);
 
-		return $img;
-	}
+        return $img;
+    }
 
-	/**
-	 * Automatically adjust image orientation
-	 * @param string $file
-	 * @param resource img
-	 * @return resource
-	 */
-	public function adjustImageOrientation($file, $img){
+    /**
+     * Automatically adjust image orientation
+     * @param string $file
+     * @param resource img
+     * @return resource
+     */
+    public function adjustImageOrientation($file, $img){
 
-		$exif = @exif_read_data($file);
+        $exif = @exif_read_data($file);
 
-		if( !$exif
-			OR !isset($exif['Orientation'])
-			OR empty($exif['Orientation']) ){
-			return $img;
-		}
+        if( !$exif
+            OR !isset($exif['Orientation'])
+            OR empty($exif['Orientation']) ){
+            return $img;
+        }
 
-		switch( $exif['Orientation'] ){
-			case 3:
-				$img = imagerotate($img, 180, 0);
-			break;
-			case 6:
-				$img = imagerotate($img, -90, 0);
-			break;
-			case 8:
-				$img = imagerotate($img, 90, 0);
-			break;
-		}
+        switch( $exif['Orientation'] ){
+            case 3:
+                $img = imagerotate($img, 180, 0);
+            break;
+            case 6:
+                $img = imagerotate($img, -90, 0);
+            break;
+            case 8:
+                $img = imagerotate($img, 90, 0);
+            break;
+        }
 
-		return $img;
-	}
+        return $img;
+    }
 
-	/**
-	 * Add transparent background to image
-	 * @param int $width
-	 * @param int $height
-	 * @return resource
-	 */
-	public function createTransparentImage($width, $height){
+    /**
+     * Add transparent background to image
+     * @param int $width
+     * @param int $height
+     * @return resource
+     */
+    public function createTransparentImage($width, $height){
 
-		$img = imagecreatetruecolor($width, $height);
-		imagealphablending($img, TRUE);
+        $img = imagecreatetruecolor($width, $height);
+        imagealphablending($img, TRUE);
 
-		$colorTransparent = imagecolorallocatealpha($img, 0, 0, 0, 127);
-		imagefill($img, 0, 0, $colorTransparent);
-		imagesavealpha($img, TRUE);
+        $colorTransparent = imagecolorallocatealpha($img, 0, 0, 0, 127);
+        imagefill($img, 0, 0, $colorTransparent);
+        imagesavealpha($img, TRUE);
 
-		return $img;
-	}
+        return $img;
+    }
 
-	/**
-	 * Resize image
-	 * @param int $newWidth
-	 * @param int $newHeight
-	 * @param string $option
-	 * @return void
-	 */
-	public function resizeImage($newWidth, $newHeight, $option = 'auto'){
+    /**
+     * Resize image
+     * @param int $newWidth
+     * @param int $newHeight
+     * @param string $option
+     * @return void
+     */
+    public function resizeImage($newWidth, $newHeight, $option = 'auto'){
 
-		$optionArray = $this->getDimensions($newWidth, $newHeight, $option);
-		$optimalWidth = $optionArray['optimalWidth'];
-		$optimalHeight = $optionArray['optimalHeight'];
+        $optionArray = $this->getDimensions($newWidth, $newHeight, $option);
+        $optimalWidth = $optionArray['optimalWidth'];
+        $optimalHeight = $optionArray['optimalHeight'];
 
-		$this->imageResized = $this->createTransparentImage($optimalWidth, $optimalHeight);
+        $this->imageResized = $this->createTransparentImage($optimalWidth, $optimalHeight);
 
-		imagecopyresampled(
-			$this->imageResized,
-			$this->image,
-			0, 0, 0, 0,
-			$optimalWidth, $optimalHeight,
-			$this->width, $this->height
-		);
+        imagecopyresampled(
+            $this->imageResized,
+            $this->image,
+            0, 0, 0, 0,
+            $optimalWidth, $optimalHeight,
+            $this->width, $this->height
+        );
 
-		if( $option == 'crop' ){
-			$this->crop($optimalWidth, $optimalHeight, $newWidth, $newHeight);
-		}
+        if( $option == 'crop' ){
+            $this->crop($optimalWidth, $optimalHeight, $newWidth, $newHeight);
+        }
 
-	}
+    }
 
-	/**
-	 * Retrieve optimal dimensions for image
-	 * @param int $newWidth
-	 * @param int $newHeight
-	 * @param string $option
-	 * @return array
-	 */
-	private function getDimensions($newWidth, $newHeight, $option){
+    /**
+     * Retrieve optimal dimensions for image
+     * @param int $newWidth
+     * @param int $newHeight
+     * @param string $option
+     * @return array
+     */
+    private function getDimensions($newWidth, $newHeight, $option){
 
-	   switch( $option ){
-			case 'exact':
-				$optimalWidth = $newWidth;
-				$optimalHeight= $newHeight;
-			break;
-			case 'portrait':
-				$optimalWidth = $this->getSizeByFixedHeight($newHeight);
-				$optimalHeight= $newHeight;
-			break;
-			case 'landscape':
-				$optimalWidth = $newWidth;
-				$optimalHeight= $this->getSizeByFixedWidth($newWidth);
-			break;
-			case 'auto':
-				$optionArray = $this->getSizeByAuto($newWidth, $newHeight);
-				$optimalWidth = $optionArray['optimalWidth'];
-				$optimalHeight = $optionArray['optimalHeight'];
-			break;
-			case 'crop':
-				$optionArray = $this->getOptimalCrop($newWidth, $newHeight);
-				$optimalWidth = $optionArray['optimalWidth'];
-				$optimalHeight = $optionArray['optimalHeight'];
-			break;
-		}
+       switch( $option ){
+            case 'exact':
+                $optimalWidth = $newWidth;
+                $optimalHeight= $newHeight;
+            break;
+            case 'portrait':
+                $optimalWidth = $this->getSizeByFixedHeight($newHeight);
+                $optimalHeight= $newHeight;
+            break;
+            case 'landscape':
+                $optimalWidth = $newWidth;
+                $optimalHeight= $this->getSizeByFixedWidth($newWidth);
+            break;
+            case 'auto':
+                $optionArray = $this->getSizeByAuto($newWidth, $newHeight);
+                $optimalWidth = $optionArray['optimalWidth'];
+                $optimalHeight = $optionArray['optimalHeight'];
+            break;
+            case 'crop':
+                $optionArray = $this->getOptimalCrop($newWidth, $newHeight);
+                $optimalWidth = $optionArray['optimalWidth'];
+                $optimalHeight = $optionArray['optimalHeight'];
+            break;
+        }
 
-		return array(
-			'optimalWidth' => $optimalWidth,
-			'optimalHeight' => $optimalHeight
-		);
-	}
+        return array(
+            'optimalWidth' => $optimalWidth,
+            'optimalHeight' => $optimalHeight
+        );
+    }
 
-	/**
-	 * Retrieve size by fixed height
-	 * @param int $newHeight
-	 * @return int
-	 */
-	private function getSizeByFixedHeight($newHeight){
-		$ratio = $this->width / $this->height;
-		$newWidth = $newHeight * $ratio;
-		return $newWidth;
-	}
+    /**
+     * Retrieve size by fixed height
+     * @param int $newHeight
+     * @return int
+     */
+    private function getSizeByFixedHeight($newHeight){
+        $ratio = $this->width / $this->height;
+        $newWidth = $newHeight * $ratio;
+        return $newWidth;
+    }
 
-	/**
-	 * Retrieve size by fixed width
-	 * @param int $newWidth
-	 * @return int
-	 */
-	private function getSizeByFixedWidth($newWidth){
-		$ratio = $this->height / $this->width;
-		$newHeight = $newWidth * $ratio;
-		return $newHeight;
-	}
+    /**
+     * Retrieve size by fixed width
+     * @param int $newWidth
+     * @return int
+     */
+    private function getSizeByFixedWidth($newWidth){
+        $ratio = $this->height / $this->width;
+        $newHeight = $newWidth * $ratio;
+        return $newHeight;
+    }
 
-	/**
-	 * Retrieve automatic size by width and height
-	 * @param int $newWidth
-	 * @param int $newHeight
-	 * @return array
-	 */
-	private function getSizeByAuto($newWidth, $newHeight){
+    /**
+     * Retrieve automatic size by width and height
+     * @param int $newWidth
+     * @param int $newHeight
+     * @return array
+     */
+    private function getSizeByAuto($newWidth, $newHeight){
 
-		// Image to be resized is wider (landscape)
-		if( $this->height < $this->width ){
-			$optimalWidth = $newWidth;
-			$optimalHeight= $this->getSizeByFixedWidth($newWidth);
+        // Image to be resized is wider (landscape)
+        if( $this->height < $this->width ){
+            $optimalWidth = $newWidth;
+            $optimalHeight= $this->getSizeByFixedWidth($newWidth);
 
-		// Image to be resized is taller (portrait)
-		}elseif( $this->height > $this->width ){
-			$optimalWidth = $this->getSizeByFixedHeight($newHeight);
-			$optimalHeight= $newHeight;
+        // Image to be resized is taller (portrait)
+        }elseif( $this->height > $this->width ){
+            $optimalWidth = $this->getSizeByFixedHeight($newHeight);
+            $optimalHeight= $newHeight;
 
-		// Image to be resizerd is a square
-		}else{
+        // Image to be resized is a square
+        }else{
 
-			if( $newHeight < $newWidth ){
-				$optimalWidth = $newWidth;
-				$optimalHeight= $this->getSizeByFixedWidth($newWidth);
+            if( $newHeight < $newWidth ){
+                $optimalWidth = $newWidth;
+                $optimalHeight= $this->getSizeByFixedWidth($newWidth);
 
-			}elseif( $newHeight > $newWidth ){
-				$optimalWidth = $this->getSizeByFixedHeight($newHeight);
-				$optimalHeight= $newHeight;
+            }elseif( $newHeight > $newWidth ){
+                $optimalWidth = $this->getSizeByFixedHeight($newHeight);
+                $optimalHeight= $newHeight;
 
-			}else{
-				$optimalWidth = $newWidth;
-				$optimalHeight= $newHeight;
-			}
+            }else{
+                $optimalWidth = $newWidth;
+                $optimalHeight= $newHeight;
+            }
 
-		}
+        }
 
-		return array(
-			'optimalWidth' => $optimalWidth,
-			'optimalHeight' => $optimalHeight
-		);
-	}
+        return array(
+            'optimalWidth' => $optimalWidth,
+            'optimalHeight' => $optimalHeight
+        );
+    }
 
-	/**
-	 * Retrieve optimal crop dimensions
-	 * @param int $newWidth
-	 * @param int $newHeight
-	 * @return array
-	 */
-	private function getOptimalCrop($newWidth, $newHeight){
+    /**
+     * Retrieve optimal crop dimensions
+     * @param int $newWidth
+     * @param int $newHeight
+     * @return array
+     */
+    private function getOptimalCrop($newWidth, $newHeight){
 
-		$heightRatio = $this->height / $newHeight;
-		$widthRatio = $this->width / $newWidth;
+        $heightRatio = $this->height / $newHeight;
+        $widthRatio = $this->width / $newWidth;
 
-		if( $heightRatio < $widthRatio ){
-			$optimalRatio = $heightRatio;
-		} else {
-			$optimalRatio = $widthRatio;
-		}
+        if( $heightRatio < $widthRatio ){
+            $optimalRatio = $heightRatio;
+        } else {
+            $optimalRatio = $widthRatio;
+        }
 
-		$optimalHeight = $this->height / $optimalRatio;
-		$optimalWidth = $this->width / $optimalRatio;
+        $optimalHeight = $this->height / $optimalRatio;
+        $optimalWidth = $this->width / $optimalRatio;
 
-		return array(
-			'optimalWidth' => $optimalWidth,
-			'optimalHeight' => $optimalHeight
-		);
-	}
+        return array(
+            'optimalWidth' => $optimalWidth,
+            'optimalHeight' => $optimalHeight
+        );
+    }
 
-	/**
-	 * Crop image
-	 * @param int $optimalWidth
-	 * @param int $optimalHeight
-	 * @param int $newWidth
-	 * @param int $newHeight
-	 * @return void
-	 */
-	public function crop($optimalWidth, $optimalHeight, $newWidth, $newHeight){
+    /**
+     * Crop image
+     * @param int $optimalWidth
+     * @param int $optimalHeight
+     * @param int $newWidth
+     * @param int $newHeight
+     * @return void
+     */
+    public function crop($optimalWidth, $optimalHeight, $newWidth, $newHeight){
 
-		// Find center - this will be used for the crop
-		$cropStartX = ( $optimalWidth / 2 ) - ( $newWidth / 2 );
-		$cropStartY = ( $optimalHeight / 2 ) - ( $newHeight / 2 );
-		$crop = $this->imageResized;
+        // Find center - this will be used for the crop
+        $cropStartX = ( $optimalWidth / 2 ) - ( $newWidth / 2 );
+        $cropStartY = ( $optimalHeight / 2 ) - ( $newHeight / 2 );
+        $crop = $this->imageResized;
 
-		// Now crop from center to exact requested size
-		$this->imageResized = $this->createTransparentImage($newWidth , $newHeight);
+        // Now crop from center to exact requested size
+        $this->imageResized = $this->createTransparentImage($newWidth , $newHeight);
 
-		imagecopyresampled(
-			$this->imageResized,
-			$crop,
-			0, 0, $cropStartX, $cropStartY,
-			$newWidth, $newHeight,
-			$newWidth, $newHeight
-		);
+        imagecopyresampled(
+            $this->imageResized,
+            $crop,
+            0, 0, $cropStartX, $cropStartY,
+            $newWidth, $newHeight,
+            $newWidth, $newHeight
+        );
 
-	}
+    }
 
-	/**
-	 * Add watemark to image
-	 * @param string $file
-	 * @return void
-	 */
-	public function addWatemark($file){
+    /**
+     * Add watermark to image
+     * @param string $file
+     * @return void
+     */
+    public function addWatermark($file){
 
-		$watermark = $this->openImage($file);
-		$watermarkWidth = imagesx($watermark);
-		$watermarkHeight = imagesy($watermark);
+        $watermark = $this->openImage($file);
+        $watermarkWidth = imagesx($watermark);
+        $watermarkHeight = imagesy($watermark);
 
-		$destX = (imagesx($this->imageResized) - $watermarkWidth) / 2;
-		$destY = (imagesy($this->imageResized) - $watermarkHeight) / 2;
+        $destX = (imagesx($this->imageResized) - $watermarkWidth) / 2;
+        $destY = (imagesy($this->imageResized) - $watermarkHeight) / 2;
 
-		imagecopy(
-			$this->imageResized,
-			$watermark,
-			$destX, $destY, 0, 0,
-			$watermarkWidth,
-			$watermarkHeight
-		);
+        imagecopy(
+            $this->imageResized,
+            $watermark,
+            $destX, $destY, 0, 0,
+            $watermarkWidth,
+            $watermarkHeight
+        );
 
-		imagedestroy($watermark);
-	}
+        imagedestroy($watermark);
+    }
 
-	/**
-	 * Save image
-	 * @param string $savePath
-	 * @param int $imageQuality
-	 * @return void
-	 */
-	public function saveImage($savePath, $imageQuality = 100){
+    /**
+     * Save image
+     * @param string $savePath
+     * @param int $imageQuality
+     * @return void
+     */
+    public function saveImage($savePath, $imageQuality = 100){
 
-		$extension = strrchr($savePath, '.');
-		$extension = strtolower($extension);
+        $extension = strrchr($savePath, '.');
+        $extension = strtolower($extension);
 
-		switch( $extension ){
-			case '.jpg':
-			case '.jpeg':
-				imagejpeg($this->imageResized, $savePath, $imageQuality);
-			break;
-			case '.gif':
-				imagegif($this->imageResized, $savePath);
-			break;
-			case '.png':
+        switch( $extension ){
+            case '.jpg':
+            case '.jpeg':
+                imagejpeg($this->imageResized, $savePath, $imageQuality);
+            break;
+            case '.gif':
+                imagegif($this->imageResized, $savePath);
+            break;
+            case '.png':
 
-				$scaleQuality = round(($imageQuality/100) * 9);
-				$invertScaleQuality = 9 - $scaleQuality;
+                $scaleQuality = round(($imageQuality/100) * 9);
+                $invertScaleQuality = 9 - $scaleQuality;
 
-				imagepng($this->imageResized, $savePath, $invertScaleQuality);
-			break;
-			default:
-				// No extension - No save.
-			break;
-		}
+                imagepng($this->imageResized, $savePath, $invertScaleQuality);
+            break;
+            default:
+                // No extension - No save.
+            break;
+        }
 
-		imagedestroy($this->imageResized);
-	}
+        imagedestroy($this->imageResized);
+    }
 
 }
